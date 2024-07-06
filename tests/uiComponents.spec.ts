5ce48acda22eb9df4412209d5110d7ef1190c79a
Linkinr <kovalchukruslanlink@gmail.com>
2024-07-06 20:05:49 +0300

    add_dropdowns_test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -36,4 +36,30 @@ test.describe('Form Layout page', () => {
         expect(await usingTheGridForm.getByLabel('Option 1').isChecked()).toBeFalsy()
         expect(await usingTheGridForm.getByLabel('Option 2').isChecked()).toBeTruthy()
     })
+
+    test('List and dropdowns', async({page}) => {
+        const dropDownMenu = page.locator('ngx-header nb-select')
+        await dropDownMenu.click()
+
+        const optionList = page.getByRole('list').locator('nb-option')
+        await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+        await optionList.filter({hasText: "Cosmic"}).click()
+        const header = page.locator('nb-layout-header')
+        await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+        const colors = {
+            "Light": 'rgb(255, 255, 255)',
+            "Dark": 'rgb(34, 43, 69)',
+            "Cosmic": 'rgb(50, 50, 89)',
+            "Corporate": 'rgb(255, 255, 255)'
+        }
+
+        await dropDownMenu.click()
+        for(const color in colors){
+            await optionList.filter({hasText: color}).click()
+            await expect(header).toHaveCSS('background-color', colors[color])
+            if(color != "Corporate")
+                await dropDownMenu.click()
+        }
+    })
 })
