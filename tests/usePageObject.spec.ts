2c08e347946a062cc06e3bf56b6a11ce8fa88c9a
Linkinr <kovalchukruslanlink@gmail.com>
2024-07-07 16:47:40 +0300

    add_datepicker_page_object

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { NavigationPage } from '../page-objects/navigationPage';
 import { FormLayoutPage } from '../page-objects/formLayoutPage';
+import { DatepickerPage } from '../page-objects/datepickerPage';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -19,8 +20,12 @@ test('navigate to Form page', async ({ page }) => {
 test('parametrized methods', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutPage = new FormLayoutPage(page)
-
+    const onDatePickerPage = new DatepickerPage(page)
     await navigateTo.formsLayoutPage()
     await onFormLayoutPage.submitUsingGridFormWithCredentialsAndSelectOptions('test@test.com', 'password', 'Option 1')
     await onFormLayoutPage.submitInlineForm('test name', 'test@test.com', true)
+    await navigateTo.datePickerPage()
+    await onDatePickerPage.selectCommonDatePickerDateFromToday(3)
+    await onDatePickerPage.selectDatepickerWithRangeFromToday(5, 15)
 })
+
