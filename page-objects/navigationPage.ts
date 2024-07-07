e98b9ed136a0d608cb81735e3f0f4ec809b5fc94
Linkinr <kovalchukruslanlink@gmail.com>
2024-07-07 16:01:22 +0300

    parametrized_methods

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -7,7 +7,34 @@ export class NavigationPage {
     }
 
     async formsLayoutPage(){
-        await this.page.getByText('Forms').click()
+        await this.selectGoupMenuItem('Forms')
         await this.page.getByText('Form Layouts').click()
+    }
+
+    async datePickerPage(){
+        await this.selectGoupMenuItem('Forms')
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async smartTablePage(){
+        await this.selectGoupMenuItem('Tables & Data')
+        await this.page.getByText('Smart Table').click()
+    }
+
+    async toasterPage(){
+        await this.selectGoupMenuItem('Modal & Overlays')
+        await this.page.getByText('Toastr').click()
+    }
+
+    async tooltipPage(){
+        await this.selectGoupMenuItem('Modal & Overlays')
+        await this.page.getByText('Tooltip').click()
+    }
+
+    private async selectGoupMenuItem(groupItemTitle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expendState = await groupMenuItem.getAttribute('aria-expanded')
+        if(expendState == "false") 
+            await groupMenuItem.click()
     }
 }
--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,12 +1,26 @@
 import { test, expect } from '@playwright/test';
 import { NavigationPage } from '../page-objects/navigationPage';
+import { FormLayoutPage } from '../page-objects/formLayoutPage';
 
-test.beforeEach(async({page}) =>{
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
 
 })
 
-test('navigate to Form page', async({page}) => {
+test('navigate to Form page', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formsLayoutPage()
+    await navigateTo.datePickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toasterPage()
+    await navigateTo.tooltipPage()
+})
+
+test('parametrized methods', async ({ page }) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutPage = new FormLayoutPage(page)
+
+    await navigateTo.formsLayoutPage()
+    await onFormLayoutPage.submitUsingGridFormWithCredentialsAndSelectOptions('test@test.com', 'password', 'Option 1')
+    await onFormLayoutPage.submitInlineForm('test name', 'test@test.com', true)
 })
